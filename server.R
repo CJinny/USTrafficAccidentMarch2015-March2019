40c69e49a61003dd0ad9c3bb675e7422ee6bff07
Jinny <cuijinjincui4@gmail.com>
2019-09-11 18:05:23 -0400

    1

---
+++ server.R
@@ -0,0 +1,63 @@
+library(shinydashboard)
+library(shiny)
+library(Hmisc)
+library(rsconnect)
+
+
+icons <- awesomeIcons(icon = "whatever",
+                      iconColor = "black",
+                      library = "ion",
+                      markerColor = acd$Color)
+
+function(input, output, session) {
+    rt <- reactive({
+        x = acd[as_date(acd$Start_Time)==input$Date,]
+        
+        if (input$State!='All') {
+            if (input$Weather!='All') {
+                df0 = subset(x, State==input$State & Weather_Condition==input$Weather)
+            } else {
+                df0 = subset(x, State==input$State)
+            }
+        } else {
+            if (input$Weather!='All') {
+                df0 = subset(x, Weather_Condition==input$Weather)
+            } else {
+                df0 = x
+            }
+        }
+        
+        if (input$County!='All') {
+            df1 = subset(df0, County==input$County)
+        } else {
+            df1 = df0
+        }
+        
+        
+        if (input$DayNight != 'Both') {
+            df2 = subset(df1, Sunrise_Sunset == input$DayNight)
+        } else {
+            df2 = df1
+        }
+        
+        df = subset(df2, Severity >= input$Severity[1] & Severity <= input$Severity[2] &
+                        Visibility.mi. >= input$Visibility[1] & Visibility.mi. <= input$Visibility[2] &
+                        Wind_Speed.mph. >= input$WindSpeed[1] & Wind_Speed.mph. <= input$WindSpeed[2]) 
+        df
+    })
+    
+    output$numRec <- renderText({
+        paste(dim(rt())[1], " Records found")
+    })
+    
+    output$pointMap <- renderLeaflet({
+        leaflet(rt()) %>%
+            addProviderTiles(providers$CartoDB.Positron) %>%
+            addAwesomeMarkers(~Start_Lng, ~Start_Lat, icon=icons, popup = ~htmlEscape(Description))
+    })
+    output$heatMap <- renderLeaflet({
+        leaflet(rt()) %>%
+            addProviderTiles(providers$CartoDB.Positron) %>%
+            addHeatmap(lng = ~Start_Lng, lat= ~Start_Lat, blur = 20, max=0.05, radius=15) 
+    })
+}
---
+++ ui.R
@@ -0,0 +1,69 @@
+library(shinydashboard)
+library(shiny)
+library(Hmisc)
+library(rsconnect)
+library(data.table)
+
+
+acd = fread('US_Accidents.csv', sep=',')
+
+STATES = c("All", sort(as.vector(unique(acd$State))))
+WEATHERCOND = c("All", sort(as.vector(unique(acd$Weather_Condition))))
+COUNTIES = c("All", sort(as.vector(unique(acd$County))))
+CITIES = c("All", sort(as.vector(unique(acd$City))))
+DAYNIGHT = c("Both", "Day", "Night")
+
+
+header = dashboardHeader(
+    title = "Traffic Accident in US from March 2015 to March 2019",
+    titleWidth = 500
+)
+
+body <- dashboardBody(
+    
+    tags$head(tags$style(HTML('
+        .skin-blue .main-header .logo {
+          background-color: #3c8dbc;
+        }
+        .skin-blue .main-header .logo:hover {
+          background-color: #3c8dbc;
+        }
+      '))),
+    
+    fluidRow(
+        column(width=9,
+               box(width = NULL, solidHeader = FALSE,
+                   textOutput("numRec")),
+               box(width = NULL, solidHeader = FALSE,
+                   title = "Pointmap",
+                   leafletOutput("pointMap", height = 400)
+               ),
+               box(width=NULL, solidHeader = TRUE,
+                   title = "Heatmap",
+                   leafletOutput("heatMap", height= 400)
+               )
+        ),
+        column(width = 3,
+               box(width = NULL, status = "warning", solidHeader = TRUE,
+                   dateInput('Date', "Please Select a Date: ", 
+                             min='2015-03-09', max='2019-04-01', value='2019-01-01'),
+                   selectInput("DayNight", "Please Select Day/Night: ", choices = DAYNIGHT),
+                   selectInput("State", "Please Select a State: ", choices = STATES),
+                   selectInput("Weather", "Please Select a Weather Condition: ", choices = WEATHERCOND),
+                   selectInput("County", "Please Select a County: ", choices = COUNTIES),
+                   sliderInput("Visibility", "Please Select Visibility (mi) Range: ", 
+                               min=0, max=10, value=c(0,10)),
+                   sliderInput("WindSpeed", "Please Select Wind Speed (mph) Range:", 
+                               min=0, max=17.3, value=c(0,17.3)),
+                   sliderInput("Severity", "Please Select Severity Range: ",
+                               min=0, max=4, value=c(0,4))
+               )
+        )
+    )
+)
+
+dashboardPage(
+    header,
+    dashboardSidebar(disable = TRUE),
+    body
+)
